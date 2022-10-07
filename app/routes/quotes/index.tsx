5885fc506655cd9ae596002f3b8d11efcef10433
ElMayo <mic.l.meier@gmail.com>
2022-10-07 08:19:49 +0200

    feat: Display random quote in quotes/index route

--- app/routes/quotes/index.tsx
+++ app/routes/quotes/index.tsx
@@ -1,8 +1,33 @@
-const QuotesIndexRoute = () => (
-  <div>
-    <p>Here's a random quote:</p>
-    <p>Insert random quote</p>
-  </div>
-)
+import type { Quote } from '@prisma/client'
+import type { LoaderFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+import { db } from '~/utils/db.server'
+
+type LoaderData = { randomQuote: Quote }
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const count = await db.quote.count()
+  const randomRowNumber = Math.floor(Math.random() * count)
+  const [randomQuote] = await db.quote.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  })
+  if (!randomQuote) throw new Error('Quote not found')
+  const data: LoaderData = { randomQuote }
+  return json(data)
+}
+
+const QuotesIndexRoute = () => {
+  const { randomQuote } = useLoaderData<LoaderData>()
+
+  return (
+    <div>
+      <p>{randomQuote.content}</p>
+      <p>{randomQuote.author}</p>
+    </div>
+  )
+}
 
 export default QuotesIndexRoute
